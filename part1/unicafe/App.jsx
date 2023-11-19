1668337e434ccaff402e6d8bd7b367eeb8839df1
Thizh <s22010229@ousl.lk>
2023-11-19 17:04:40 +0530

    unicafe step2

--- part1/unicafe/App.jsx
+++ part1/unicafe/App.jsx
@@ -10,6 +10,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -20,6 +22,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good - bad) / all}</p>
+      <p>positive {good / all * 100} %</p>
     </div>
   )
 }
