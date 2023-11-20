eeecabd18913bada3cc534953124f83dc01e100b
Thizh <s22010229@ousl.lk>
2023-11-20 12:06:06 +0530

    unicafe step6

--- part1/unicafe/App.jsx
+++ part1/unicafe/App.jsx
@@ -10,19 +10,27 @@ const Statistics = (props) => {
   }
 
   return (
-    <>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.all} />
-      <StatisticLine text="average" value={props.average} />
-      <StatisticLine text="positive" value={props.positive} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive} />
+      </tbody>
+    </table>
   )
 }
 
 const StatisticLine = (props) => {
-  return <p>{props.text} {props.value}</p>
+
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
 }
 
 const App = () => {
